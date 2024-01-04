3bb2a2a7389fafde243edfca49acf01bc9d61ed7
Maxim <moroz@nonstudio.ru>
2024-01-05 00:54:31 +0300

    Initial Commit

---
+++ Skillbox-SwiftUI-M1/Skillbox_SwiftUI_M1App.swift
@@ -0,0 +1,17 @@
+//
+//  Skillbox_SwiftUI_M1App.swift
+//  Skillbox-SwiftUI-M1
+//
+//  Created by Максим Морозов on 05.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Skillbox_SwiftUI_M1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
