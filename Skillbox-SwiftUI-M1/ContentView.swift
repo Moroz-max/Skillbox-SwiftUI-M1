194ff6b4ee66f38db1c9905a22dd216cb63dbdff
Maxim <moroz@nonstudio.ru>
2024-01-05 01:04:34 +0300

    first commit

--- Skillbox-SwiftUI-M1/ContentView.swift
+++ Skillbox-SwiftUI-M1/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alpha = 0.0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("Roman")
+                .opacity(alpha)
+                .onAppear {
+                    let animation = Animation.easeInOut(duration: 3)
+                    withAnimation(animation) {
+                        alpha = 1
+                    }
+                }
         }
         .padding()
     }
